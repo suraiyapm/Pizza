db14a157a8e1221eea4a8cc99469e2ca4057dfca
suraiyapm <s.pobmark@gmail.com>
2025-09-15 19:15:11 -0400

    initial

--- pizza.py
+++ pizza.py
@@ -6,3 +6,12 @@ X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
 
 print("X: \n ",X)
 print("Y: \n ",Y)
+
+sns.set()
+plt.axis([0, 50, 0, 50])
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.xlabel("Reservations". fontsize=30)
+plt.ylabel("Pizzas", fontsize=30)
+plt.plot (X, Y, "bo")
+plt.show()
