6fb3dfb056c636d4f5eeef866bcd3acbf203f29a
suraiyapm <s.pobmark@gmail.com>
2025-09-15 20:27:22 -0400

    bias

--- pizza.py
+++ pizza.py
@@ -46,3 +46,49 @@ print("num pizzas: ", y_hat)
 def loss(X, Y, w, b=0):
     return np.average((predict(X,w,b)-Y)**2)
 # Another name for Y_hat: predict(X,w,b). For all the Xs, take the average of Y
+# Our training strat is guess, calculate loss, and use that information to improve the guess by a learning rate. Last function for the "Training Phase":
+
+def train(X,Y, iterations, lr):
+    w = 0
+    for i in range (interations):
+        current_loss = loss(X,Y, w)
+        print("Iterations %4d => Loss: %.6f" % (i, current_loss))
+        if loss(X,Y, w + lr) < current_loss:
+            w += lr
+            # learning rate is step size to changing line in this algorithm
+        elif loss(X,Y, w - lr) < current_loss:
+            w -= lr
+            # subtracting that learning rate from w
+            # Hyperparameter to training data: learning rate (lr)
+        else:
+            return w
+            # can't get anymore minimal by changing w by the learning rate in both pos and neg directions, found minimum and return w.
+            # if we don't return w, by the time iterations done, couldn't minimize, and raise Exception.
+            # Iterations is a hyperparameter you have to tweak. Want to learn and play with it a little bit
+    raise Exception("Couldn't converge within %d iterations" % iterations)
+
+w = train(X,Y, 10000, .01)
+print("\nw =%.3f" % w)
+print("Prediction: x=%d => y=%.2f" % (20, predict(20,w)))
+
+# End of naive ML model
+# Left the bias out, so not too accurate.
+
+def train_b(X,Y, iterations, lr):
+    w = b = 0
+    for i in range (iterations):
+        current_loss = loss(X,Y, w, b)
+        print("Iterations %4d => Loss: %.6f" % (i, current_loss))
+        if loss(X,Y, w+lr, b) < current_loss:
+            w += lr
+        elif loss(X,Y, w-lr, b) < current_loss:
+            w -= lr
+        elif loss(X,Y, w, b+lr) < current_loss:
+            b += lr
+        elif loss(X,Y, w, b-lr) < current_loss:
+            b -= lr
+        else:
+            return w, b
+    raise Exception("Couldn't converge within %d iterations" % iterations)
+# All in the book
+# Before, only 183 iterations. With bias, 1500.
